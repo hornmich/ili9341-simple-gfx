f5b85def273bf9998fb877e57804c281d52b01be
Michal Horn <michal@apartman.cz>
2020-08-07 19:28:56 +0200

    Move some files to a submodule

--- fonts/arial_reg_12_img.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ARIAL_REG_12_H
-#define __ARIAL_REG_12_H
-
-#define ARIAL_REG_12_WIDTH 128
-#define ARIAL_REG_12_HEIGHT 128
-
-extern unsigned char arial_reg_12_bits[];
-
-#endif /* __ARIAL_REG_12_H */
